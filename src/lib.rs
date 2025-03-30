1f6ca6c09eb41d4c96f60c223f04b1b2893ced4d
reshane <shanekenny023@gmail.com>
2025-03-29 23:14:33 -0400

    support delete

--- src/lib.rs
+++ src/lib.rs
@@ -21,6 +21,7 @@ use std::{collections::HashMap, sync::Arc, sync::Mutex};
 use tokio::net::TcpListener;
 use tower_http::services::ServeDir;
 use axum::Json;
+use axum::routing::delete;
 
 // state type
 #[derive(Debug)]
@@ -62,6 +63,23 @@ async fn data_get(
     }
 }
 
+async fn data_delete(
+    Path((_data_type, id)): Path<(String, i64)>,
+    State(state): State<Arc<AuthrState>>,
+) -> impl IntoResponse {
+    match state.store.lock() {
+        Ok(mut store) => {
+            match store.delete(id) {
+                Some(data) => Json(data).into_response(),
+                None => AuthrError::NotFound.into_response(),
+            }
+        },
+        Err(_) => {
+            (StatusCode::INTERNAL_SERVER_ERROR, "could not get data from store").into_response()
+        },
+    }
+}
+
 async fn data_create(
     Path(data_type): Path<DataType>,
     State(state): State<Arc<AuthrState>>,
@@ -87,6 +105,7 @@ async fn data_create(
 fn data_routes(state: Arc<AuthrState>) -> Router {
     Router::new()
         .route("/{type}/{id}", get(data_get))
+        .route("/{type}/{id}", delete(data_delete))
         .route("/{type}", post(data_create))
         .with_state(state)
 }
